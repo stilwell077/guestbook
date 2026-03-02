208b459b91cd94ad762aad7fe1c86031c89efe41
Svyatoslav Prudnik <learning@email.com>
2026-03-02 22:54:41 +0200

    added controller, migration, setting up db

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,21 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name = ''): Response
     {
+                $greet = '';
+                if ($name) {
+                    $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+                }
                 return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-            EOF
+                <html>
+                    <body>
+                        $greet
+                        <img src="/images/under-construction.gif" />
+                    </body>
+                </html>
+                EOF
         );
     }
 }
