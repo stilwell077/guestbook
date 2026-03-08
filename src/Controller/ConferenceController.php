57afbd162444339374096081f911956a15a28055
Svyatoslav Prudnik <learning@email.com>
2026-03-08 12:30:25 +0200

    fixed bug with sessions table, added TwigEventSubscriber

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -21,11 +21,12 @@ final class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{id}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return $this->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
